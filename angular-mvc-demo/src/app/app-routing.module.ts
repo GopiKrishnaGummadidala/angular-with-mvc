114eeafd197cfba6e5714b8abe06f4ddc078c7ec
Gopi <gopikrishna.gummadidala@gmail.com>
2021-05-29 19:51:30 +0200

    routing added

--- angular-mvc-demo/src/app/app-routing.module.ts
+++ angular-mvc-demo/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from "./product/product.component";
+import { SampleComponent } from "./sample/sample.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "sample", component: SampleComponent },
+  { path: "product", component: ProductComponent },
+  { path: "", redirectTo: "sample", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
