a3eaef72a5fe33d18bb1a797f22da77cceb7c85f
Gopi <gopikrishna.gummadidala@gmail.com>
2021-05-29 19:52:01 +0200

    routing actions defined

--- angular-mvc-demo/src/app/product/product.component.ts
+++ angular-mvc-demo/src/app/product/product.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.scss"],
 })
 export class ProductComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  backToSample(): void {
+    this.router.navigate(["/sample"]);
   }
-
 }
--- angular-mvc-demo/src/app/sample/sample.component.ts
+++ angular-mvc-demo/src/app/sample/sample.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sample',
-  templateUrl: './sample.component.html',
-  styleUrls: ['./sample.component.scss']
+  selector: "app-sample",
+  templateUrl: "./sample.component.html",
+  styleUrls: ["./sample.component.scss"],
 })
 export class SampleComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  backToProduct(): void {
+    this.router.navigate(["/product"]);
   }
-
 }
